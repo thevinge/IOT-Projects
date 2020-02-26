7ce40bb29bc9da8b2379b547523ac005fa1d3cf3
JV <jving16@student.sdu.dk>
2020-02-26 11:15:04 +0100

    Handin -Temperature Sensor Calibration - Init

---
+++ IOT-Projects/Temperature-Sensor-Calibration/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Temperature_Sensor_Calibration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
