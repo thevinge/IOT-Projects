c6722e80f6b5e27c09f352c62a8c78ec2cc7bcee
JV <jving16@student.sdu.dk>
2020-02-26 11:00:47 +0100

    Full Circle Latency Project added

---
+++ IOT-Projects/IOT-Full-Circle-Latency/Communication/SignalReceiver.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO.Ports;
+
+namespace IOT_Full_Circle_Latency.Communication
+{
+    public class SignalReceiver
+    {
+        private SerialPort sp;
+        public List<DateTime> dateTimes { get; set; }
+
+        public SignalReceiver(string port)
+        {
+            sp = new SerialPort(port);
+        }
+
+
+        public void Start()
+        {
+            sp.BaudRate = 115200;
+            sp.ReadTimeout = 5000;
+            sp.Open();
+
+            try
+            {
+                while (true)
+                {
+                    var result = sp.ReadLine();
+                    
+                    if (string.Compare(result, "1", CultureInfo.CurrentCulture, CompareOptions.IgnoreSymbols) == 0)
+                    {
+                        dateTimes.Add(DateTime.Now);
+                    }
+                }
+            }
+            catch (TimeoutException e)
+            {
+                sp.Close();
+                sp.Dispose();
+                Console.WriteLine("Timeout");
+                //Do not do shit   
+            }
+        }
+    }
+}
---
+++ IOT-Projects/IOT-Full-Circle-Latency/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IOT_Full_Circle_Latency
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
