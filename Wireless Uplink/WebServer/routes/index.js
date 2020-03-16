3c79f2f2e7d619b61c061dd99d2104ead16df310
frederik <frede.madsen3@gmail.com>
2020-03-16 11:16:36 +0100

    fixes to the webserver so it can store stuff in the database

--- Wireless Uplink/WebServer/routes/index.js
+++ Wireless Uplink/WebServer/routes/index.js
@@ -5,7 +5,6 @@ var db = require('../database');
 router.post('/create', function(req, res, next) {
     let name = req.body.name;
     db.one("INSERT INTO tuser (name) values ($1) RETURNING id,name", name)
-        .timeout()
         .then(data => {
             res.status(200).json(data);
         })
@@ -17,22 +16,16 @@ router.post('/create', function(req, res, next) {
 
 
 router.post('/', function(req, res, next) {
-    console.table(req.body);
     let temp = req.body.temp;
     let light = req.body.light;
     let reqno = req.body.reqno;
-    console.table({temp, light, reqno});
-    console.table(db);
     db.none("INSERT INTO tiot (temp,light,reqno) values ($1, $2, $3)", [temp, light, reqno])
-        .then(() => {
-            console.log("got here 1?");
+        .then(data => {
             res.status(200).send();
         })
         .catch(err => {
-            console.log("got here 2?");
             res.status(500).send(err)
         });
-    console.log("got here 3?");
 });
 
 
